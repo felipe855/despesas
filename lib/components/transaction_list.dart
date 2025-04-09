e42d59431126a4581cf55751c28bc89e773cc074
Felp117 <felipe.8129@aluno.pr.senac.br>
2025-04-09 16:08:52 -0300

    projeto completo de despesas

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,64 @@
+import '../models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+
+  const TransactionList(this.transactions, this.onRemove,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 600,
+      child:
+          transactions.isEmpty
+              ? Column(
+                children: [
+                  Text(
+                    'Nenhuma Transação cadastrada',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              )
+              : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (ctx, index) {
+                  final tr = transactions[index];
+                  return Card(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: FittedBox(child: Text('R\$${tr.value}')),
+                        ),
+                      ),
+                      title: Text(
+                        tr.title,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      subtitle: Text(DateFormat('d MMM y').format(tr.date)),
+                      trailing: IconButton(
+                        onPressed: () => onRemove(tr.id),
+                        icon: Icon(Icons.delete),
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  );
+                },
+              ),
+    );
+  }
+}
